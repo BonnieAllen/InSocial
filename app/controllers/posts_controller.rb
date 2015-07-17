77230fdcf52c2c99cf3e453927f91c4e10023a8e
Bonnie <aalle.allen@gmail.com>
2015-07-17 14:37:48 -0400

    Added Simple form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,21 @@
 class PostsController < ApplicationController
 	def index
 	end
+	
+	def new
+		@post = Post.create
+	end
+
+	def create
+		@post = Post.create(post_params)
+		redirect_to_posts_path
+	end
+	
+	private
+
+	def post_params
+		params.require(:post).permit(:image, :supertitle)
+	end
+
+
 end
