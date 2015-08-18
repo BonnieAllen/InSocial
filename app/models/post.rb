4be2b0dd175864c6c3a6c8c5c6e6b00a1ed6882b
Bonnie <aalle.allen@gmail.com>
2015-08-18 17:55:59 -0400

    model order update

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,14 @@
 class Post < ActiveRecord::Base
+  has_attached_file :image, styles: { :medium => "640x" }
+  has_many :comments, dependent: :destroy  
+  has_many :uploads
   validates :image, presence: true
   validates :user_id, presence: true
 
-  has_many :comments, dependent: :destroy  
-  has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   belongs_to :user 
   
-  has_many :uploads
+
   # accepts_nested_attributes_for :style_models, :allow_destroy => true
 
 end
--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,8 +1,8 @@
 class Upload < ActiveRecord::Base
+  has_attached_file :uploaded_file, styles: { original: "4000x4000>", large: "1500x1500>", medium: "640x645>", small: "200x200>", thumb: "160x160>" }, default_url: "/images/:style/missing.png"
   # validates :image_url, presence: true
   validates :uploaded_file, presence: true
   # has_attached_file :uploaded_file
-  has_attached_file :uploaded_file, styles: { original: "4000x4000>", large: "1500x1500>", medium: "640x645>", small: "200x200>", thumb: "160x160>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :uploaded_file, 
   content_type: [
   "image/jpg", 
