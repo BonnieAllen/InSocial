21581da181d11cf55c5541c9c727ed5fa551fc20
Bonnie <aalle.allen@gmail.com>
2015-08-18 21:48:59 -0400

    deleted app

--- app/assets/javascripts/uploads.js
+++
@@ -1,4 +0,0 @@
-var loadFile = function(event) {  
-  var output = document.getElementById('uploaded_file-preview');
-  output.src = URL.createObjectURL(event.target.files[0]);
-};
--- app/views/image_scaffolds/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @image_scaffold, :id, :title, :description, :image, :created_at, :updated_at
